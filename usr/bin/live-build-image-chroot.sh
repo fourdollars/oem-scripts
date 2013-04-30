2d09308274d47e19960cb106464c39f23d5653d9
Rex Tsai <rex.tsai@canonical.com>
2013-04-30 16:40:12 +0800

    * usr/bin/live-build-image-chroot.sh  - Now support to genreate a new squashfs image.

--- usr/bin/live-build-image-chroot.sh
+++ usr/bin/live-build-image-chroot.sh
@@ -1,51 +1,54 @@
 #!/bin/sh
+# Canonical (C) 2012-2013
+# FourDollars aka Shih-Yuan Lee <sylee@canonical.com>
+# Rex Tsai <rex.tsai@canonical.com>
 
-NAME='live-build-image-chroot'
 TARGET="$1"
-
-[ ! -f "$TARGET" ] && exit 0
-
-exec 3>$NAME.diff
-
-TMPDIR="$(mktemp -d)"
-if echo $TARGET | grep img$ ; then
-    sudo mount -o ro,offset=512 $TARGET $TMPDIR
-else
-    sudo mount -o ro $TARGET $TMPDIR
+NAME=$(basename $TARGET)
+CMD=$2
+OUTPUTDIR=${3-`pwd`}
+if [ ! -f "$TARGET" ] || [ -z $CMD ] ; then
+    echo "$0: image.[img|squashfs] [diff|tar|squashfs]"
+    exit 0
 fi
 
-if [ -f $TMPDIR/casper/filesystem.squashfs ]; then
-    LOOPDIR="$(mktemp -d)"
+TMPDIR="$(mktemp -d)"
+LOOPDIR="$(mktemp -d)"
+DIFFDIR="$(mktemp -d)"
+AUFSDIR="$(mktemp -d)"
+if head $TARGET | file - | grep "x86 boot sector" ; then
+    sudo mount -o ro,offset=512 $TARGET $TMPDIR
     sudo mount -r $TMPDIR/casper/filesystem.squashfs $LOOPDIR
-    DIFFDIR="$(mktemp -d)"
-    AUFSDIR="$(mktemp -d)"
     sudo mount -t aufs -o br:$DIFFDIR:$LOOPDIR none $AUFSDIR
     if file $AUFSDIR/bin/ls | grep ARM >/dev/null 2>&1; then
-        ARM="1"
         sudo cp /usr/bin/qemu-arm-static $AUFSDIR/usr/bin/
     fi
-    cd $AUFSDIR
 else
-    cd $TMPDIR
+    sudo mount -o ro $TARGET $LOOPDIR
+    sudo mount -t aufs -o br:$DIFFDIR:$LOOPDIR none $AUFSDIR
+    if file $AUFSDIR/bin/ls | grep ARM >/dev/null 2>&1; then
+        sudo cp /usr/bin/qemu-arm-static $AUFSDIR/usr/bin/
+    fi
 fi
 
 export LANG=C LANGUAGE=C
-sudo cp /etc/resolv.conf $AUFSDIR/etc/
 
 sudo chroot $AUFSDIR /bin/bash
-cd -
 
-if [ -f $TMPDIR/casper/filesystem.squashfs ]; then
-    if [ -f $AUFSDIR/usr/bin/qemu-arm-static ]; then
-        sudo rm $AUFSDIR/usr/bin/qemu-arm-static
-    fi
-    sudo umount $AUFSDIR && rmdir $AUFSDIR
-    cd $DIFFDIR
-    if echo $* | grep tar >/dev/null 2>&1; then
-        sudo tar cJf $OLDPWD/$NAME.tar.xz *
-    fi
-    if echo $* | grep diff >/dev/null 2>&1; then
-        for file in $(sudo find -type f -print | grep -v './.wh..wh' | grep -v 'resolv.conf'); do
+if [ -f $AUFSDIR/usr/bin/qemu-arm-static ]; then
+    sudo rm $AUFSDIR/usr/bin/qemu-arm-static
+fi
+
+# Generate diff tarball or diff or new squashfs for testing.
+cd $DIFFDIR
+case ${CMD} in
+    "tar")
+        sudo tar cJf ${OUTPUTDIR}/${NAME}.tar.xz *
+        ;;
+    "diff")
+        exec 3> ${OUTPUTDIR}/$NAME.diff
+
+        for file in $(sudo find -type f -print | grep -v './.wh..wh'); do
             if [ -f $LOOPDIR/$file ]; then
                 a="$LOOPDIR/$file"
             else
@@ -54,10 +57,14 @@ if [ -f $TMPDIR/casper/filesystem.squashfs ]; then
             b="$DIFFDIR/$file"
             sudo diff -u $a $b | sed "s,$LOOPDIR/.,a," | sed "s,$DIFFDIR/.,b," >&3
         done
-    fi
-    cd -
-    sudo umount $LOOPDIR && rmdir $LOOPDIR
-    sudo rm -fr $DIFFDIR
-fi
+        ;;
+    "squashfs")
+        mksquashfs $AUFSDIR ${OUTPUTDIR}/$NAME.squashfs.new -info
+        ;;
+esac
+
+sudo umount $AUFSDIR && rmdir $AUFSDIR
+sudo umount $LOOPDIR && rmdir $LOOPDIR
+sudo rm -fr $DIFFDIR
 
 sudo umount $TMPDIR && rmdir $TMPDIR
