28c01b7dee6f79a8551acdb749c5be0ce3402c6c
Alex Tu <alex.tu@canonical.com>
2021-08-25 22:39:42 +0800

    verified on both somerville and stella image

--- recovery-from-iso.sh
+++ recovery-from-iso.sh
@@ -4,6 +4,7 @@ set -ex
 jenkins_job_for_iso=""
 jenkins_job_build_no="lastSuccessfulBuild"
 script_on_target_machine="inject_recovery_from_iso.sh"
+additional_grub_for_ubuntu_recovery="99_ubuntu_recovery"
 user_on_target="ubuntu"
 SSH="ssh -o StrictHostKeyChecking=no"
 SCP="scp -o StrictHostKeyChecking=no"
@@ -116,6 +117,61 @@ apt-get install -fy
     # get pkgs for ssh key and skip disk checking.
     $GIT clone https://git.launchpad.net/~oem-solutions-engineers/pc-enablement/+git/oem-fix-misc-cnl-misc-for-automation --depth 1 misc_for_automation
 
+    if [ "${ubr}" == "yes" ]; then
+    mkdir -p "$temp_folder"/preseed
+    cat <<EOF1 > "$temp_folder/preseed/$additional_grub_for_ubuntu_recovery"
+#!/bin/bash -e
+source /usr/lib/grub/grub-mkconfig_lib
+cat <<EOF
+menuentry "ubuntu-recovery restore" --hotkey f9 {
+        search --no-floppy --hint '(hd0,gpt2)' --set --fs-uuid UUID_OF_RECOVERY_PARTITION
+        if [ -s /boot/grub/common.cfg ]; then
+            source /boot/grub/common.cfg
+        else
+            set options="boot=casper automatic-ubiquity noprompt quiet splash"
+        fi
+
+        if [ "\\\${grub_platform}" != "efi" ]; then
+            if [ -f \\\${prefix}/nomodesetlist.txt ]; then
+                if hwmatch \\\${prefix}/nomodesetlist.txt 3; then
+                    if [ \\\${match} != 0 ]; then
+                        set options="nomodeset \\\$options"
+                    fi
+                fi
+            fi
+        fi
+
+        #Support starting from a loopback mount (Only support ubuntu.iso for filename)
+        if [ -f /ubuntu.iso ]; then
+            loopback loop /ubuntu.iso
+            set root=(loop)
+            set options="\\\$options iso-scan/filename=/ubuntu.iso"
+        fi
+        if [ -n "\\\${lang}" ]; then
+            set options="\\\$options locale=\\\$lang"
+        fi
+
+        linux   /casper/vmlinuz ubuntu-recovery/recovery_type=hdd \\\$options
+        initrd  /casper/initrd
+}
+EOF
+EOF1
+    cat <<EOF > "$temp_folder/preseed/set_env_for_ubuntu_recovery"
+#!/bin/bash -ex
+# replace the grub entry which ubuntu_recovery expected
+recover_p=\$(lsblk -l | grep efi | cut -d ' ' -f 1 | sed 's/.$/2'/)
+UUID_OF_RECOVERY_PARTITION=\$(ls -l /dev/disk/by-uuid/ | grep vda2 | awk '{print \$9}')
+echo partition = \$UUID_OF_RECOVERY_PARTITION
+sed -i "s/UUID_OF_RECOVERY_PARTITION/\$UUID_OF_RECOVERY_PARTITION/" push_preseed/preseed/$additional_grub_for_ubuntu_recovery
+sudo rm -f /etc/grub.d/99_dell_recovery || true
+chmod 766 push_preseed/preseed/$additional_grub_for_ubuntu_recovery
+sudo cp push_preseed/preseed/$additional_grub_for_ubuntu_recovery /etc/grub.d/
+
+# Force changing the recovery partition label to PQSERVICE for ubuntu-recovery
+sudo fatlabel /dev/\$recover_p PQSERVICE
+EOF
+    fi
+
     return 0
 }
 push_preseed() {
@@ -152,6 +208,10 @@ inject_preseed() {
     download_preseed && \
     push_preseed
     scp -o StrictHostKeyChecking=no "$user_on_target"@"$target_ip":/cdrom/SUCCSS_push_preseed "$temp_folder" || usage
+
+    if [ "${ubr}" == "yes" ]; then
+        $SSH "$user_on_target"@"$target_ip" bash \$HOME/push_preseed/preseed/set_env_for_ubuntu_recovery || usage
+    fi
     $SSH "$user_on_target"@"$target_ip" touch /tmp/SUCCSS_inject_preseed
 }
 
@@ -172,7 +232,8 @@ cat <<EOF > "$temp_folder/$script_on_target_machine"
 set -ex
 sudo umount /cdrom /mnt || true
 sudo mount -o loop $img_name /mnt && \
-sudo mount /dev/\$(lsblk -l | grep efi | cut -d ' ' -f 1 | sed 's/.$/2'/) /cdrom && \
+recover_p=\$(lsblk -l | grep efi | cut -d ' ' -f 1 | sed 's/.$/2'/) && \
+sudo mount /dev/\$recover_p /cdrom && \
 df | grep "cdrom\|mnt" | awk '{print \$2" "\$6}' | sort | tail -n1 | grep -q cdrom && \
 sudo mkdir -p /var/tmp/rsync && \
 sudo rsync -alv /mnt/ /cdrom/ $rsync_opts && \
@@ -212,7 +273,9 @@ poll_recovery_status() {
 
 do_recovery() {
     if [ "${ubr}" == "yes" ]; then
-        echo GRUB_DEFAULT=3 | $SSH "$user_on_target"@"$target_ip" -T "sudo tee -a /etc/default/grub.d/automatic-oem-config.cfg"
+        echo GRUB_DEFAULT='"ubuntu-recovery restore"' | $SSH "$user_on_target"@"$target_ip" -T "sudo tee -a /etc/default/grub.d/automatic-oem-config.cfg"
+        echo GRUB_TIMEOUT_STYLE=menu | $SSH "$user_on_target"@"$target_ip" -T "sudo tee -a /etc/default/grub.d/automatic-oem-config.cfg"
+        echo GRUB_TIMEOUT=5 | $SSH "$user_on_target"@"$target_ip" -T "sudo tee -a /etc/default/grub.d/automatic-oem-config.cfg"
         $SSH "$user_on_target"@"$target_ip" sudo update-grub
         $SSH "$user_on_target"@"$target_ip" sudo reboot &
     else
