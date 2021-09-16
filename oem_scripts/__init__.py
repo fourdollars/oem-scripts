4ec9da82db6eb187063fe6c70bb40d552c5088f2
Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
2021-09-16 14:49:10 +0800

    mir-bug, oem-meta-packages, oem_scripts/__init__.py: Resue the _run_command.

--- oem_scripts/__init__.py
+++ oem_scripts/__init__.py
@@ -1,3 +1,25 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021  Canonical Ltd.
+# Author: Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import subprocess
+from logging import debug, info, critical
+
 __version__ = "1.14"
 
 ALLOWED_KERNEL_META_LIST = (
@@ -30,3 +52,35 @@ def yes_or_ask(yes: bool, message: str) -> bool:
             return True
         else:
             return False
+
+
+def _run_command(
+    command: list or tuple, returncode=(0,), env=None, silent=False
+) -> (str, str, int):
+    if not silent:
+        debug("$ " + " ".join(command))
+    proc = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env
+    )
+    out, err = proc.communicate()
+
+    if out:
+        out = out.decode("utf-8").strip()
+    if err:
+        err = err.decode("utf-8").strip()
+
+    if proc.returncode not in returncode:
+        critical(f"return {proc.returncode}")
+        if out:
+            info(out)
+        if err:
+            critical(err)
+        exit(1)
+
+    if not silent:
+        if out:
+            debug(out)
+        if err:
+            debug(err)
+
+    return (out, err, proc.returncode)
