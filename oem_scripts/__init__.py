4a2870ed18133e797b2a091d8c8bfd61639845ed
Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
2022-06-23 10:57:12 +0800

    releasing package oem-scripts version 1.43

--- oem_scripts/__init__.py
+++ oem_scripts/__init__.py
@@ -25,7 +25,7 @@ import subprocess
 from logging import debug, info, error, critical
 from tempfile import TemporaryDirectory
 
-__version__ = "1.42"
+__version__ = "1.43"
 
 ALLOWED_KERNEL_META_LIST = (
     "linux-oem-20.04d",
