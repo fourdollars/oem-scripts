63c22500c1de00f8bdd34722dfcde20dcc886b26
Kai-Chuan Hsieh <kaichuan.hsieh@canonical.com>
2023-07-24 16:56:57 +0800

    setup-apt-dir.sh: Add support for mantic

--- setup-apt-dir.sh
+++ setup-apt-dir.sh
@@ -179,7 +179,7 @@ case "${CODENAME}" in
     (xenial|bionic|focal)
         PUBKEY="790BC7277767219C42C86F933B4FE6ACC0B21F32"
         ;;
-    (hirsute|impish|jammy)
+    (hirsute|impish|jammy|mantic)
         PUBKEY="F6ECB3762474EDA9D21B7022871920D1991BC93C"
         ;;
     (*)
